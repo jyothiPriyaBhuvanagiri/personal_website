da11669a222baae5babb8a938f09438e6a4fb43d
Jyothipriya Bhuvanagiri <108977425+jyothiPriyaBhuvanagiri@users.noreply.github.com>
2024-06-27 21:43:48 +0200

    adding skills.js to my portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/Navbar';
+import {Banner} from './components/Banner'
+import {Skills} from './components/skills'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 function App() {
   return (
     <div className="App">
-
+<NavBar></NavBar>
+        <Banner></Banner>
+        <Skills></Skills>
     </div>
   );
 }
