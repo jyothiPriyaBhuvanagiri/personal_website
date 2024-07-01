28f33295bebc782e9c44cd649b82ad38d3afe6a4
Jyothipriya Bhuvanagiri <108977425+jyothiPriyaBhuvanagiri@users.noreply.github.com>
2024-07-01 22:01:54 +0200

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/Navbar';
 import {Banner} from './components/Banner'
 import {Skills} from './components/skills'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import {Projects} from "./components/projects";
+import {ProjectCard} from "./components/ProjectCard";
+
 function App() {
   return (
     <div className="App">
 <NavBar></NavBar>
         <Banner></Banner>
         <Skills></Skills>
+        <Projects></Projects>
+
     </div>
   );
 }
