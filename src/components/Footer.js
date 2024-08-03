7363d025acb8e6696474fce7bb6ac2c379449682
Jyothipriya Bhuvanagiri <108977425+jyothiPriyaBhuvanagiri@users.noreply.github.com>
2024-08-03 22:11:06 +0200

    Making app work again with our other dependency's

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../asset/img/nav-icon1.svg";
+import navIcon2 from "../asset/img/nav-icon2.svg";
+import navIcon3 from "../asset/img/nav-icon3.svg";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+
+                    <Col size={12} sm={7} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="https://github.com/jyothiPriyaBhuvanagiri" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon2} alt="GitHub" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/jyothi-priya-a93a8b205" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon1} alt="LinkedIn" />
+                            </a>
+                        </div>
+                        <p className="rights">Copyright 2024. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
